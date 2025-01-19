21517bdbc0edfaec2e8fac93450941384e1bae24
Gathala <agathala@3c22fbe1e6ff.ant.amazon.com>
2025-01-19 15:22:20 -0800

    Adding E01

---
+++ topic-01/e01.py
@@ -0,0 +1,12 @@
+#separated by spaces
+print("1 2 3")
+#separated by commas
+print("1,2,3")
+# each in a separate line - option 1
+print("1")
+print("2")
+print("3")
+# each in a separate line - option 2
+print("1\n2\n3")
+
+
